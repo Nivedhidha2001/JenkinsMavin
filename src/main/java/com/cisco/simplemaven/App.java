f4a6f07f4b81054cb94e95906a8272cd4b2ecda2
Nivedhidha Sathiyaseelan (nisathiy) <nisathiy@cisco.com>
2024-02-29 12:09:34 +0530

    first commit

---
+++ src/main/java/com/cisco/simplemaven/App.java
@@ -0,0 +1,14 @@
+package com.cisco.simplemaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.print("Welcome to jenkins");
+    }
+}
