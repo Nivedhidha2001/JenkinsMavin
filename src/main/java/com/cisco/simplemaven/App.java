5c44b8ffcb0db522622b060e3bcd91cb82eb0df4
Nivedhidha Sathiyaseelan (nisathiy) <nisathiy@cisco.com>
2024-02-29 15:12:34 +0530

    changed file

--- src/main/java/com/cisco/simplemaven/App.java
+++ src/main/java/com/cisco/simplemaven/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+    	HelloWorld.print();
         System.out.println( "Hello World!" );
         System.out.print("Welcome to jenkins");
     }
---
+++ src/main/java/com/cisco/simplemaven/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.cisco.simplemaven;
+
+public class HelloWorld {
+	
+	public static void print() {
+		System.out.println("Hello from CI");
+	}
+	
+	
+
+}
